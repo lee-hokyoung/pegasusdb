8a893f291c12eecaa099f4bb13f016afa94bd3de
super <supereggsong@gmail.com>
2020-02-03 19:14:42 +0900

    asus commit

---
+++ public/assets/js/comm_value.js
@@ -0,0 +1,80 @@
+const __category = [
+  {
+    cate: "Beauty",
+    sub_cate: ["Hair Salon", "SPA", "Nail Shop"]
+  },
+  {
+    cate: "Living",
+    sub_cate: ["Infortainment", "Home appliances", "Interior"]
+  },
+  {
+    cate: "Health",
+    sub_cate: [
+      "Adult care & Support",
+      "Silver care & Support",
+      "Baby care & Support"
+    ]
+  },
+  {
+    cate: "Transport",
+    sub_cate: ["Smart Mobility", "Drone", "Public Transportation"]
+  },
+  {
+    cate: "Food & Beverage",
+    sub_cate: ["Restaurant & Cafe", "Delivery", "Franchise"]
+  },
+  {
+    cate: "E-Commerce",
+    sub_cate: ["PC & Mobile shopping", "Consumer", "Producer"]
+  }
+];
+const __cities = [
+  "베이징",
+  "청두",
+  "장사",
+  "상하이",
+  "항저우",
+  "선양",
+  "광저우",
+  "우한",
+  "칭다오",
+  "선전",
+  "충칭",
+  "정저우",
+  "난징",
+  "다렌",
+  "홍콩",
+  "텐진",
+  "동관",
+  "마카오",
+  "쑤저우",
+  "닝보",
+  "대만",
+  "시안",
+  "충칭"
+];
+const __region = [
+  "안후이성",
+  "허난성",
+  "칭하이성",
+  "푸젠성",
+  "후베이성",
+  "산시성",
+  "간쑤성",
+  "후난성",
+  "산둥성",
+  "구이저우성",
+  "장쑤성",
+  "산시성",
+  "하이난성",
+  "장시성",
+  "쓰촨성",
+  "허베이성",
+  "지린성",
+  "원난성",
+  "헤이룽장성",
+  "랴오닝성",
+  "저장성",
+  "칭하이성",
+  "광동성"
+];
--- public/assets/js/list.js
+++ public/assets/js/list.js
@@ -1,90 +1,89 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // Category 선택시
-  $('select[name="category"]').on('change', function (o) {
-    console.log('option : ', o.currentTarget);
-    let path_obj = location.pathname.split('/');
+  $('select[name="category"]').on("change", function(o) {
+    console.log("option : ", o.currentTarget);
+    let path_obj = location.pathname.split("/");
     let path_len = path_obj.length;
     path_obj[path_len - 1] = o.currentTarget.value;
-    let pathname = path_obj.join('/');
+    let pathname = path_obj.join("/");
     let strQuery = fnGetQuery();
-    history.pushState(null, '', pathname + strQuery);
+    history.pushState(null, "", pathname + strQuery);
   });
 });
-const fnSearchDetail = function () {
+const fnSearchDetail = function() {
   let strQuery = fnGetQuery();
   let xhr = new XMLHttpRequest();
-  xhr.open('GET', '/ajax' + location.pathname + strQuery, true);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onreadystatechange = function () {
+  xhr.open("GET", "/ajax" + location.pathname + strQuery, true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function() {
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-      history.pushState(null, '', strQuery);
+      history.pushState(null, "", strQuery);
       fnGenerateHtmlResult(JSON.parse(this.response));
     }
   };
   xhr.send();
 };
 // list 결과 HTML 생성하는 함수
-const fnGenerateHtmlResult = function (res) {
-  let cate = res.cate, cate_item = res.cate_item;
-  let list = document.querySelector('.list-group');
-  list.innerHTML = '';
-  res.list.forEach(function (v) {
-    let a = document.createElement('a');
-    a.className = 'list-group list-group-item-action flex-row my-1 py-2';
-    a.href = '/detail/' + cate + '/' + cate_item + '/' + v._id;
-    a.setAttribute('style', 'min-height:1px;');
-    let div = document.createElement('div');
-    div.className = 'col-2 text-center';
-    let img = document.createElement('img');
-    img.classList.add('p-1');
-    if(v.chart_type)
-      img.src = '/assets/images/' + v.chart_type + '_user.JPG';
-    else
-      img.src = '/assets/images/slide6.jpg';
-    img.alt = '';
-    img.setAttribute('style', 'width:70%;');
+const fnGenerateHtmlResult = function(res) {
+  let cate = res.cate,
+    cate_item = res.cate_item;
+  let list = document.querySelector(".list-group");
+  list.innerHTML = "";
+  res.list.forEach(function(v) {
+    let a = document.createElement("a");
+    a.className = "list-group list-group-item-action flex-row my-1 py-2";
+    a.href = "/detail/" + cate + "/" + cate_item + "/" + v._id;
+    a.setAttribute("style", "min-height:1px;");
+    let div = document.createElement("div");
+    div.className = "col-2 text-center";
+    let img = document.createElement("img");
+    img.classList.add("p-1");
+    if (v.chart_type) img.src = "/assets/images/" + v.chart_type + "_user.JPG";
+    else img.src = "/assets/images/slide6.jpg";
+    img.alt = "";
+    img.setAttribute("style", "width:70%;");
     div.appendChild(img);
     a.appendChild(div);
-    div = document.createElement('div');
-    div.className = 'col-10 d-flex flex-column justify-content-around';
-    let h5 = document.createElement('h5');
+    div = document.createElement("div");
+    div.className = "col-10 d-flex flex-column justify-content-around";
+    let h5 = document.createElement("h5");
     h5.innerText = v.data_title;
     div.appendChild(h5);
-    let div_1 = document.createElement('div');
-    div_1.className = 'py-2 overflow-hidden';
-    div_1.id = 'info_' + v._id;
+    let div_1 = document.createElement("div");
+    div_1.className = "py-2 overflow-hidden";
+    div_1.id = "info_" + v._id;
     // category array
     let category_arr = [];
-    Object.keys(v.category_obj).forEach(function (m) {
+    Object.keys(v.category_obj).forEach(function(m) {
       if (v.category_obj[m].length > 0) {
-        v.category_obj[m].forEach(function (w) {
+        v.category_obj[m].forEach(function(w) {
           if (w[Object.keys(w)]) category_arr.push(w[Object.keys(w)]);
         });
       }
     });
-    let span = document.createElement('span');
-    span.className = 'font-weight-bold text-secondary';
-    span.innerText = category_arr.join(',');
+    let span = document.createElement("span");
+    span.className = "font-weight-bold text-secondary";
+    span.innerText = category_arr.join(",");
     div_1.appendChild(span);
     // bar add
-    let span_bar = document.createElement('span');
-    span_bar.className = 'text-secondary';
-    span_bar.innerHTML = '&nbsp;&nbsp;|&nbsp;&nbsp;';
+    let span_bar = document.createElement("span");
+    span_bar.className = "text-secondary";
+    span_bar.innerHTML = "&nbsp;&nbsp;|&nbsp;&nbsp;";
     div_1.appendChild(span_bar);
     // region add
     let region = v.region_array.concat(v.city_array);
-    span = document.createElement('span');
-    span.className = 'font-weight-bold text-secondary';
-    span.innerText = region.join(',');
+    span = document.createElement("span");
+    span.className = "font-weight-bold text-secondary";
+    span.innerText = region.join(",");
     div_1.appendChild(span);
     // bar add
-    let span_bar_1 = document.createElement('span');
-    span_bar_1.className = 'text-secondary';
-    span_bar_1.innerHTML = '&nbsp;&nbsp;|&nbsp;&nbsp;';
+    let span_bar_1 = document.createElement("span");
+    span_bar_1.className = "text-secondary";
+    span_bar_1.innerHTML = "&nbsp;&nbsp;|&nbsp;&nbsp;";
     div_1.appendChild(span_bar_1);
     // object add
-    span = document.createElement('span');
-    span.className = 'font-weight-bold text-secondary';
+    span = document.createElement("span");
+    span.className = "font-weight-bold text-secondary";
     span.innerText = v.object;
     div_1.appendChild(span);
     div.appendChild(div_1);
@@ -92,41 +91,45 @@ const fnGenerateHtmlResult = function (res) {
     list.appendChild(a);
     // 길어질 경우, 더보기/접기 버튼 추가
     if (category_arr.length + region.length > 10) {
-      div_1.className += ' showMore';
-      let showMore = document.createElement('a');
-      showMore.className = 'hidden-wrap text-right';
-      showMore.href = '#';
-      showMore.setAttribute('data-toggle', 'showMore');
-      showMore.setAttribute('data-target', '#info_' + v._id);
-      showMore.setAttribute('data-expanded', 'false');
+      div_1.className += " showMore";
+      let showMore = document.createElement("a");
+      showMore.className = "hidden-wrap text-right";
+      showMore.href = "#";
+      showMore.setAttribute("data-toggle", "showMore");
+      showMore.setAttribute("data-target", "#info_" + v._id);
+      showMore.setAttribute("data-expanded", "false");
       list.appendChild(showMore);
     }
   });
 };
 // 쿼리 스트링 파싱 함수
-const fnGetQuery = function () {
+const fnGetQuery = function() {
   let query = [];
-  document.querySelectorAll('select.add_query').forEach(function (s) {
-    if (s.selectedIndex > 0) query.push(s.name + '=' + s.value);
+  document.querySelectorAll("select.add_query").forEach(function(s) {
+    if (s.selectedIndex > 0) query.push(s.name + "=" + s.value);
   });
-  return '?' + query.join('&');
+  return "?" + query.join("&");
 };
 // 긴 문장 토글
-$(document).on('click', 'a[data-toggle="showMore"]', function (e) {
+$(document).on("click", 'a[data-toggle="showMore"]', function(e) {
   let target_id = e.currentTarget.dataset.target.toString();
   let target = document.querySelector(target_id);
   let toggle = e.currentTarget.dataset.expanded;
-  e.currentTarget.dataset.expanded = (toggle === 'false');
-  if (toggle === 'false') target.classList.remove('showMore');
-  else target.classList.add('showMore');
+  e.currentTarget.dataset.expanded = toggle === "false";
+  if (toggle === "false") target.classList.remove("showMore");
+  else target.classList.add("showMore");
 });
 // Search Words
-function searchList(){
-  let searchText = document.querySelector('#searchText').value;
+function searchList() {
+  let searchText = document.querySelector("#searchText").value;
   let xhr = new XMLHttpRequest();
-  xhr.open('GET', '/ajax' + location.pathname + '/' + searchText + '?' + location.search, true);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onreadystatechange = function () {
+  xhr.open(
+    "GET",
+    "/ajax" + location.pathname + "/" + searchText + "?" + location.search,
+    true
+  );
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function() {
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
       fnGenerateHtmlResult(JSON.parse(this.response));
     }
